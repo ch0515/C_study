36cd8cede3fc9dfc1d4e71490f9fecab2b3c3b58
chaemin <w2114@e-mirim.hs.kr>
2022-10-22 21:54:58 +0900

    예외처리 추가

--- Ex7-2.c
+++ Ex7-2.c
@@ -10,8 +10,14 @@ int main(void) {
 }
 int get_num(void) { //void 를 넣어 매개변수가 없음을 표시
 	int num;
+
 	printf("양수 입력 : ");
 	scanf("%d", &num);
+	while (num < 0) {
+		printf("양수를 입력하세요!\n");
+		printf("양수 입력 : ");
+		scanf("%d", &num);
+	}
 
 	return num;
 }
